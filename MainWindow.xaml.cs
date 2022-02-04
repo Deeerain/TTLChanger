42feb7399f22852a4b1bfc75c1429da305b713ff
Deerain <d33.i2.1n@gmail.com>
2022-02-04 22:12:42 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TTLChanger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly TTLManager TTLManager;
+
+        public MainWindow()
+        {   
+            InitializeComponent();
+
+            try
+            {
+                TTLManager = new TTLManager();
+
+                v4TTLTextBox.Text = TTLManager.V4TTL.ToString();
+                v6TTLTextBox.Text = TTLManager.V6TTL.ToString();
+            }
+            catch (System.Security.SecurityException ex)
+            {
+                MessageBox.Show("Попробуйте запустить программу от имени администратора");
+                App.Current.Shutdown();
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var dialogResult = MessageBox.Show("Вы используете эту программу на свой страх и риск. Вы уверены, что хотите сохранить изменения?", "", MessageBoxButton.YesNo);
+
+                if(dialogResult == MessageBoxResult.Yes)
+                {
+                    if (v4TTLTextBox.Text != "-1")
+                        TTLManager.V4TTL = v4TTLTextBox.Text;
+                    if (v6TTLTextBox.Text != "-1")
+                        TTLManager.V6TTL = v6TTLTextBox.Text;
+                }
+
+                var rebootDialog = MessageBox.Show("После изменений, рекомендуется перезагрузка. Перезагрузить компьютер?", "", MessageBoxButton.YesNo);
+
+                if (rebootDialog == MessageBoxResult.Yes)
+                    RebootPC();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void RebootPC()
+        {
+            Process.Start("shutdown", "/s /t 0");
+        }
+    }
+}
---
+++ TTLManager.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+
+namespace TTLChanger
+{
+    public class TTLManager
+    {
+        private RegistryKey _tcpipKey;
+        private RegistryKey _tcpip6Key;
+        private string _defaultTTLPropertyName;
+
+        private string _v4TTL;
+        private string _v6TTL;
+
+        public TTLManager()
+        {
+            _defaultTTLPropertyName = "DefaultTTL";
+
+            _tcpipKey = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters", true);
+            _tcpip6Key = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Services\\Tcpip6\\Parameters", true);
+
+            _v4TTL = GetDefaultTTLValue(_tcpipKey);
+            _v6TTL = GetDefaultTTLValue(_tcpip6Key);
+        }
+
+        public string V4TTL
+        {
+            get => _v4TTL;
+            set => _tcpipKey.SetValue(_defaultTTLPropertyName, value, RegistryValueKind.DWord);
+        }
+
+        public string V6TTL
+        {
+            get => _v6TTL;
+            set => _tcpip6Key.SetValue(_defaultTTLPropertyName, value, RegistryValueKind.DWord);
+        }
+
+        private bool HaveDefaultTTLParameter(RegistryKey key)
+        {
+            object property = key.GetValue(_defaultTTLPropertyName);
+            return true ? property != null : false;
+        }
+    
+        private string GetDefaultTTLValue(RegistryKey key)
+        {
+            return HaveDefaultTTLParameter(key) ? key.GetValue(_defaultTTLPropertyName).ToString() : "-1";
+        }
+    }
+}
